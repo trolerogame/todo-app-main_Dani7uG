209f8fd5ed5c00d7d22c8c4d6457be621ee7d16c
Daniel Campos <danielgcampos.04@gmail.com>
2021-09-26 04:14:23 -0300

    Update FooterTodoList.jsx
    filtros arreglados

--- src/components/FooterTodoList.jsx
+++ src/components/FooterTodoList.jsx
@@ -1,21 +1,18 @@
 import React from 'react'
 import {saveLocalStorage} from '../functions/saveLocalStorage'
 import { FooterItem,TaskLeft,ClearComplete,BarFilter } from '../styles/styleFooter'
+import {getLocalStorage as getTodo} from '../functions/getLocalStorage'
 const FooterTodoList = ({darkMode,todos,setTodo}) => {
     const handleClearComplete = () => {
         const newTodo = todos.filter(item => item.check !== true)
         setTodo(newTodo)
         saveLocalStorage(newTodo)
     }
-    const all = () => setTodo(JSON.parse(localStorage.getItem('todo')))
-    const complete = () => {
-        all()
-        setTodo(todos.filter(item => item.check === true))
-    }
-    const activate = () => {
-        all()
-        setTodo(todos.filter(item => item.check === false))
-    }
+    const filteredTodo = bool => 
+        setTodo(getTodo().filter(item => item.check === bool))
+    const all = () => setTodo(getLocalStorage())
+    const complete = () => filteredTodo(true)
+    const activate = () => filteredTodo(false)
     return (
         <FooterItem darkMode={darkMode}>
             <TaskLeft darkMode={darkMode} >{todos.length} items left</TaskLeft>
